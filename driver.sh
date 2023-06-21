4d85b01ff44bb6f302651f9e91c88630cec7e903
Vignesh Sathyaseelan <98504160+vigneshS29@users.noreply.github.com>
2023-06-21 00:19:04 -0400

    Add files via upload

---
+++ driver.sh
@@ -0,0 +1,16 @@
+#!/bin/bash                                                                                 
+
+#SBATCH --job-name $1
+#SBATCH -N 1
+#SBATCH -n $2
+#SBATCH -t $3
+#SBATCH -A $4
+#SBATCH -o output.txt
+#SBATCH -e error.txt
+
+for file in $1*; do
+    # Run several instances parallely 
+    python /home/vsathyas/bin/GA/GA/driver_xtb.py -f /home/vsathyas/bin/GA/GA/$file >> data.txt & 
+done
+
+wait 
