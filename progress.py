0e0699a8d874e48f2bfe0fbd07f861d81f999099
Vignesh Sathyaseelan <98504160+vigneshS29@users.noreply.github.com>
2024-04-30 10:55:53 -0400

    Update progress.py

--- progress.py
+++ progress.py
@@ -17,40 +17,36 @@ enumerate for iterations where both index and value are needed.
 
 '''
 
+def io_progress(name, current_step, total, start, static, bar_length=50):
+    
+    percent_complete = (current_step + 1) / total * 100
+    filled_bar = int(bar_length * percent_complete / 100)
+    progress_bar = "[" + "#" * filled_bar + "-" * (bar_length - filled_bar) + "]"
+    if static: print(f" \r Progress {name} : {progress_bar} {percent_complete:.2f}% Time: {time.time() - start:.10f}s", end="") 
+    else: print(f"Progress {name} : {progress_bar} {percent_complete:.2f}% Time: {time.time() - start:.10f}s \n", end="")
+    
+    return 
+
 def prange(iterable,total=None,static=None):
     
     start = time.time()
+    
     if isinstance(iterable, enumerate):
         if total == None: raise ValueError ('Please Provide total in case of enumerate')
-        bar_length = 50  # Adjust this for desired bar length
         for current_step,item in iterable:
-            percent_complete = (current_step + 1) / total * 100
-            filled_bar = int(bar_length * percent_complete / 100)
-            progress_bar = "[" + "#" * filled_bar + "-" * (bar_length - filled_bar) + "]"
-            if static: print(f" \r Progress (Enumerate) : {progress_bar} {percent_complete:.2f}% Time: {time.time() - start:.10f}s", end="") 
-            else: print(f"Progress (Enumerate) : {progress_bar} {percent_complete:.2f}% Time: {time.time() - start:.10f}s \n", end="") 
+            io_progress('Enumerate',current_step,total,start,static)
             yield(current_step,item)
 
     elif isinstance(iterable, io.IOBase):
         iterable = iterable.readlines()
         total = len(iterable)
-        bar_length = 50  # Adjust this for desired bar length
         for current_step,item in enumerate(iterable):
-            percent_complete = (current_step + 1) / total * 100
-            filled_bar = int(bar_length * percent_complete / 100)
-            progress_bar = "[" + "#" * filled_bar + "-" * (bar_length - filled_bar) + "]"
-            if static: print(print(f" \r Progress (File Read) : {progress_bar} {percent_complete:.2f}% Time: {time.time() - start:.10f}s", end="") )
-            else:print(f"Progress (File Read) : {progress_bar} {percent_complete:.2f}% Time: {time.time() - start:.10f}s \n", end="") 
+            io_progress('File',current_step,total,start,static)
             yield(item)
       
     else:
         total = len(iterable)
-        bar_length = 50  # Adjust this for desired bar length
         for current_step,item in enumerate(iterable):
-            percent_complete = (current_step + 1) / total * 100
-            filled_bar = int(bar_length * percent_complete / 100)
-            progress_bar = "[" + "#" * filled_bar + "-" * (bar_length - filled_bar) + "]"
-            if static: print(f" \r Progress (Iterate) : {progress_bar} {percent_complete:.2f}% Time: {time.time() - start:.10f}s", end="") 
-            else: print(f"Progress (Iterate) : {progress_bar} {percent_complete:.2f}% Time: {time.time() - start:.10f}s \n", end="") 
+            io_progress('Iterable',current_step,total,start,static)
             yield(item)
     return
